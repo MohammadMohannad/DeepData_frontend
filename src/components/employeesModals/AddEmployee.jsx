3dbe95b9e548fd256b0de68111c4852cb86f1404
Dhulfiqar Ali <dhulfiqarali6@gmail.com>
2024-09-20 09:43:06 +0300

    Employees page done

--- src/components/employeesModals/AddEmployee.jsx
+++ src/components/employeesModals/AddEmployee.jsx
@@ -97,7 +97,7 @@ function AddEmployee() {
         layer="50"
       >
         <div className="w-full text-right mb-4">
-          <h2 className="text-base font-semibold">تعديل معلومات الموظف</h2>
+          <h2 className="text-base font-semibold">اضافة موظف</h2>
           <p className="text-sm text-muted-foreground">املئ الحقول الاتيه </p>
         </div>
         <form onSubmit={handleSubmit} className="w-full">
